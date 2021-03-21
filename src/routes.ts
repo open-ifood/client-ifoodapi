1853a9618370d95dc18e91a9717109c227b32db1
Leonardo Elias de Oliveira <leoelias02@hotmail.com>
2021-03-20 22:21:40 -0300

    :passport_control: middleware to control access to session

--- src/routes.ts
+++ src/routes.ts
@@ -2,11 +2,13 @@ import { Router } from 'express';
 import authController from './controller/auth-controller';
 import confirmAuthController from './controller/confirm-auth-controller';
 import notFoundController from './controller/not-found-controller';
+import authMiddleware from './middleware/auth-middleware';
 
 const routes = Router();
 
 routes.post('/auth', authController);
 routes.patch('/confirm-auth', confirmAuthController);
+routes.use(authMiddleware);
 routes.all('*', notFoundController);
 
 export default routes;
